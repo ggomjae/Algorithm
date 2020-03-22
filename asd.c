fb3e9dd59be80e55b81835dadc25bf087a55204d
ggomjae <ggomjae@naver.com>
2020-03-22 21:04:24 +0900

    2020 03 22 c언어 학생 가르친 코드

---
+++ asd.c
@@ -0,0 +1,14 @@
+   #include <stdio.h>
+   
+   int main(){
+      
+      char firstInital,secondInital,thirdInital;
+      scanf("%c",&firstInital);
+      scanf("%c",&secondInital);
+      scanf("%c",&thirdInital);
+      
+      printf("Initials of your name are %c, %c, ans %c.",firstInital,secondInital,thirdInital);
+      
+      return 0;
+   }
+   
