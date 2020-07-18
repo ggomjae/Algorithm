4ac83a39877e7f6f3cf8ced464d69fcfac0151e0
ggomjae <ggomjae@naver.com>
2020-07-18 21:53:41 +0900

    class15649 2020 07 18

--- class15649.cpp
+++ class15649.cpp
@@ -2,31 +2,35 @@
 
 using namespace std;
 
-int arr[10][10];
-
 int main(){
 	
-	int mv = -1;
-	int ansi,ansj;
+	int a,b,c;
+	cin >> a >> b >> c;
 	
-	for(int i = 1 ; i <= 9 ; i++){
-		for(int j = 1 ; j <= 9 ; ++j){
-			cin >> arr[i][j];
-		}
+	int m;
+	cin >> m;
+	
+	a += m/3600;
+	m -= (m/3600)*3600;
+	b += m/60;
+	m -= (m/60)*60;
+	c += m%60;
+	
+	if(c>= 60){
+		b += c/60;
+		c = c%60;
 	}
 	
-	for(int i = 1 ; i <= 9 ; ++i){
-		for(int j = 1 ; j <= 9 ; ++j){
-			if(arr[i][j] > mv){
-				ansi = i;
-				ansj = j;
-				mv = arr[i][j];
-			}
-		}
+	if(b>=60){
+		a += b/60;
+		b = b%60;
 	}
 	
-	cout << mv << '\n';
-	cout << ansi << " " << ansj << '\n';
+	if(a>= 24){
+		a = a%24;
+	}
+	
+	cout << a << " " << b << " " << c;
 	
 	return 0;
 }
