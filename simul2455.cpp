8daf8ff89091681609539ed249d892f81cf090b9
ggomjae <ggomjae@naver.com>
2020-07-22 22:53:17 +0900

    2020 07 22

--- simul2455.cpp
+++ simul2455.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int result;
-int ans;
+int arr[100];
+vector<int> v;
 
 int main(){
 	
+	int n,m;
+	cin >> n >> m;
 	
-	for(int i = 0 ; i < 4 ; ++i){
-		int minus,plus;
-		cin >> minus >> plus;
-		
-		result += plus;
-		result -= minus;
-		
-		ans = max(ans,result);
+	
+	for(int i = 0 ; i < n-3 ; ++i){
+		v.push_back(0);
 	}
+	for(int i = 0 ; i < 3 ; ++i){
+		v.push_back(1);
+	}
+	
+	
+	for(int i = 0 ; i < n ; ++i){
+		cin >> arr[i];
+	}
+	
+	int ans = 0;
+	int sum = 0;
+	
+	do{
+		sum = 0 ;
+		for(int i = 0 ; i < v.size() ; ++i){
+			if(v[i] == 1)
+				sum += arr[i];
+		}
+		
+		if(sum <= m)
+			ans = max(ans,sum);
+		
+		
+	}while(next_permutation(v.begin(),v.end()));
 	
 	cout << ans << '\n';
 	
